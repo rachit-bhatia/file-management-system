4aed10ca67419e47cdd2a2cae3b9e81a85ea93bb
Rachit Bhatia <rac.s903@gmail.com>
2024-12-11 23:45:56 +0800

    add template for displaying files in list view

---
+++ src/components/FileListView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div style="padding: 60px 0;">
+        <table style="width: 100%;">
+            <thead>
+                <tr>
+                    <th style="width: 40%;">| Name</th>
+                    <th>| Type</th>
+                    <th>| Last Modified</th>
+                    <th>| Size</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+      </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FileListView',
+}
+</script>
+
+<style>
+th {
+  text-align: left;
+  width: 20%;
+}
+</style>
+
