9f5d5b1ea0e7d3546c16b95b9006a6b656da00a3
Rachit Bhatia <rac.s903@gmail.com>
2024-12-22 20:40:43 +0530

    add dummy file row and create file selection UI

--- src/components/FileListView.vue
+++ src/components/FileListView.vue
@@ -1,6 +1,6 @@
 <template>
     <div style="padding: 60px 0;">
-        <table style="width: 100%;">
+        <table style="width: 100%; border-collapse: collapse;">
             <thead>
                 <tr>
                     <th style="width: 40%;">| Name</th>
@@ -10,6 +10,12 @@
                 </tr>
             </thead>
             <tbody>
+                <tr class="file-row" @click="(event) => {handleFileSelection(event)}">
+                    <td><p class="file-data">This is a test file</p></td>
+                    <td><p class="file-data">txt</p></td>
+                    <td><p class="file-data">7 December 2024</p></td>
+                    <td><p class="file-data">12 KB</p></td>
+                </tr>
             </tbody>
       </table>
     </div>
@@ -18,6 +24,29 @@
 <script>
 export default {
     name: 'FileListView',
+    props: {
+        enableActions: {
+            type: Boolean,
+            default: false
+        },
+    },
+    methods: {
+        handleFileSelection(event) {
+            var fileRow;
+
+            //accessing the <tr> row element
+            event.composedPath().forEach((element) => {
+                if (element.tagName === "TR") {
+                    fileRow = element;
+                }
+            });
+            const fileID = fileRow.id;
+            fileRow.classList.toggle('file-selected');
+
+            //enabling/disabling actions upon file selection
+            this.$emit("set-actions", fileRow.classList.contains('file-selected'));
+        }
+    }
 }
 </script>
 
@@ -25,6 +54,23 @@ export default {
 th {
   text-align: left;
   width: 20%;
+  padding-bottom: 10px;
+}
+
+tr {
+    font-size: 18px;
+}
+
+.file-data {
+    margin: 10px 0px;
+}
+
+.file-row:hover {
+    background-color: rgba(159, 159, 159, 0.4);
+}
+
+.file-selected, .file-selected:hover {
+    background-color: rgba(92, 152, 168, 0.4);
 }
 </style>
 
